b75cc2e0fffbc8f2511d13a89dd27fc49fa2adf7
Travis <ottobonn28@gmail.com>
2015-06-07 16:21:08 -0700

    Added hardware rendering toggle

--- software/src/main.c
+++ software/src/main.c
@@ -48,38 +48,42 @@ extern volatile bool samples_for_fft_requested;
 // For hardware FFT
 kiss_fft_cfg fft_cfg;
 volatile kiss_fft_cpx fft_output[FFT_LEN];
-static void fft_isr (void *context, unsigned int id);
 volatile bool audio_ready = false;
+
+// Number of bins to draw (must be <= FFT_LEN)
+#define DRAWING_BINS (FFT_LEN / 4)
 
 // FFT averaging and power spectrum
 #define AVERAGING_LENGTH 1
 size_t current_power_history_index = 0;
-double power_history[FFT_LEN/2][AVERAGING_LENGTH];
-double average_power_spectrum[FFT_LEN/2];
+double power_history[DRAWING_BINS][AVERAGING_LENGTH];
+double average_power_spectrum[DRAWING_BINS];
+
+// If true, use hardware rendering
+volatile bool use_hardware_rendering = true;
 
 /*******************************
  ****  FUNCTION PROTOTYPES  ****
  *******************************/
-/* General. */
+/* General functions */
 static int initialize (void);
 static void run (void);
 
 /* Hardware FFT functions */
-static void fft_isr (void *context, unsigned int id);
 int fft ();
 void signal_audio_ready ();
 
 /* Drawing functions */
 void draw_fft ();
 void swap_buffers ();
+void toggle_hardware_rendering ();
 
 /* Configuration functions */
 static void configure_lcd ();
 static void configure_interrupts ();
 static int configure_fft ();
 
-
-float mapf (float value, float d0, float d1, float r0, float r1);
+/* Math functions */
 double mapd (double value, double d0, double d1, double r0, double r1);
 
 /********************************
@@ -120,11 +124,15 @@ static int initialize (void)
   return 0;
 }
 
+/* Set up audio and pushbutton interrupts. */
 static void configure_interrupts ()
 {
   audio_init (audio_isr);
+  pushbuttons_enable_interrupts (pushbuttons_isr);
+  pushbuttons_set_interrupt_mask (BUTTON1);
 }
 
+/* Enable LCD double buffering and DMA, then clear the screen. */
 static void configure_lcd ()
 {
   lcd_set_front_buffer (LCD_FRONT_BUFFER);
@@ -134,6 +142,7 @@ static void configure_lcd ()
   swap_buffers ();
 }
 
+/* Allocate the FFT configuration stryct and prepare sample array. */
 static int configure_fft ()
 {
   fft_cfg = kiss_fft_alloc (FFT_LEN, 0, NULL, 0);
@@ -184,11 +193,11 @@ int fft ()
   int i, j;
   kiss_fft (fft_cfg, samples_for_fft, fft_output);
 
-  for (i = 0; i < FFT_LEN / 2; i++)
+  for (i = 0; i < DRAWING_BINS; i++)
     {
       double power = sqrt((double)fft_output[i].r*(double)fft_output[i].r + (double)fft_output[i].i*(double)fft_output[i].i);
 
-      float scaled_power = mapd(power, 0, 100000000, 0, LCD_RES_Y-1);
+      float scaled_power = mapd(power, 0, 10000000, 0, LCD_RES_Y-1);
       power_history[i][current_power_history_index] = scaled_power;
       // Blend in historical data for smoothing
       average_power_spectrum[i] = 0;
@@ -200,78 +209,63 @@ int fft ()
   return 0;
 }
 
+/* Swap the LCD buffers and pass the new backbuffer address to the TLDA. */
 void swap_buffers ()
 {
   lcd_swap_buffers ();
   tlda_set_drawing_buffer (lcd_get_backbuffer_addr ());
 }
 
+/* Draw the visualizer pattern using the FFT output. */
 void draw_fft ()
 {
   lcd_draw_rectangle_back (0, 0, LCD_RES_X, LCD_RES_Y, BLACK);
   const size_t bar_width = 12;
 
   int i;
-  for (i = 0; i < FFT_LEN / 4; i++)
+  for (i = 0; i < DRAWING_BINS; i++)
     {
       int value = (int) average_power_spectrum[i];
-      // lcd_draw_rectangle (bar_width * i, LCD_RES_Y - value, bar_width, value, rand () % 0x10000);
-      tlda_draw (bar_width * i + bar_width / 2,
-                LCD_RES_Y - value,
-                bar_width * i + bar_width / 2,
-                LCD_RES_Y,
-                (value + 1000) % 0x10000,
-                bar_width / 2
-                );
+      if (use_hardware_rendering)
+        {
+          tlda_draw (bar_width * i + bar_width / 2,
+                    LCD_RES_Y - value,
+                    bar_width * i + bar_width / 2,
+                    LCD_RES_Y,
+                    (value + 1000) % 0x10000,
+                    bar_width / 2);
+        }
+      else
+        {
+          lcd_draw_rectangle_back (bar_width * i + bar_width / 2,
+                                      LCD_RES_Y - value,
+                                      bar_width / 2,
+                                      value,
+                                      (value + 1000) % 0x10000);
+        }
     }
   swap_buffers ();
 }
 
-float mapf (float value, float d0, float d1, float r0, float r1)
-{
-  if (value > d1) return r1;
-  if (value < d0) return r0;
-  float new = value * (r1 - r0) / (d1 - d0);
-  return new;
-}
-
+/* Map a double whose domain spans from d0 to d1 onto the range given by
+ * r0 to r1. */
 double mapd (double value, double d0, double d1, double r0, double r1)
 {
   if (value > d1) return r1;
   if (value < d0) return r0;
-  double new = value * (r1 - r0) / (d1 - d0);
+  double new = (value - d0) * (r1 - r0) / (d1 - d0);
   return new;
 }
 
+/* Called by the audio ISR to signal the main loop that samples are ready
+ * for the FFT. */
 void signal_audio_ready ()
 {
   audio_ready = true;
 }
 
-static void fft_isr (void *context, unsigned int id)
+/* Turn hardware rendering on or off. */
+void toggle_hardware_rendering ()
 {
-  green_leds_set (current_power_history_index);
-}
-
-/* Cool strobe feature that brought some much needed joy during long nights in the lab */
-void clear_strobe_flash (void)
-{
-	int coin;
-	int x, y;
-	int i;
-	for (i = 0; i < 5000; i++)
-	{
-		coin = rand() % 2;
-		if (coin)
-			{
-				x = rand() % 2 ? 0 : LCD_RES_X;
-				y = rand() % (LCD_RES_Y + 1);
-			}
-		else
-			{
-				x = rand() % (LCD_RES_X + 1);
-				y = rand() % 2 ? 0 : LCD_RES_Y;
-			}
-		tlda_draw (LCD_RES_X / 2, LCD_RES_Y / 2, x, y, 0, 1);
-	}
+  use_hardware_rendering = !use_hardware_rendering;
 }
--- software/src/pushbuttons_isr.c
+++ software/src/pushbuttons_isr.c
@@ -7,5 +7,10 @@
  */
 void pushbuttons_isr (void *context, unsigned int id)
 {
+  uint32_t edges = pushbuttons_get_edge_capture ();
+  if (edges & BUTTON1)
+    {
+      toggle_hardware_rendering ();
+    }
   pushbuttons_clear_edge_capture ();
 }
